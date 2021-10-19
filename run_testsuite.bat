e0c9543f02b6264968425983d42d3130951434e7
Jane Tournois <jane.tournois@geometryfactory.com>
2021-10-19 07:29:40 -0700

    1st version of testsuite

---
+++ run_testsuite.bat
@@ -0,0 +1,1 @@
+C:/dev/cygwin64/bin/bash.exe --login /cygdrive/c/CGAL_ROOT/run_testsuite.sh
---
+++ run_testsuite.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+cd /cygdrive/c/CGAL_ROOT
+exec &> >(tee run_testsuite.log)
+#bash -$- ./update_boost.sh
+
+#if [ "$(date +"%a")" != "Fri" ] && [ "$(date +"%a")" != "Sat" ]; then
+  bash -$- ./autotest_cgal_with_ctest -s
+#fi
