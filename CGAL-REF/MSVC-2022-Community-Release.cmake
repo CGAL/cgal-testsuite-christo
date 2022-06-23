63fabfafafcdd0407c29798e3d8540301e70e0af
Jane Tournois <jane.tournois@geometryfactory.com>
2022-06-23 15:24:38 +0200

    add 3 new platforms

---
+++ CGAL-REF/MSVC-2022-Community-Release.cmake
@@ -0,0 +1,45 @@
+SET(ITK_DIR "C:/dev/InsightToolkit-5.2.0/CMake" CACHE PATH "")
+
+SET(TBB_DIR "C:/dev/tbb2019_20191006oss_win/tbb2019_20191006oss/cmake" CACHE PATH "")
+
+SET(Qt5_DIR "C:/Qt/5.15.2/msvc2015_64/lib/cmake/Qt5" CACHE PATH "")
+
+SET(EIGEN3_INCLUDE_DIR "C:/dev/eigen-3.4.0" CACHE PATH "")
+
+#SET(OSQP_INCLUDE_DIR "C:/dev/osqp/install/include" CACHE PATH "")
+
+#SET(OSQP_LIBRARIES "C:/dev/osqp/install/lib/osqp.lib" CACHE FILEPATH "")
+
+SET(GMP_INCLUDE_DIR "C:/dev/CGAL-5.3-win64-auxiliary-libraries-gmp-mpfr/auxiliary/gmp/include" CACHE PATH "")
+
+SET(GMP_LIBRARIES "C:/dev/CGAL-5.3-win64-auxiliary-libraries-gmp-mpfr/auxiliary/gmp/lib/libgmp-10.lib" CACHE FILEPATH "")
+
+SET(MPFR_INCLUDE_DIR "C:/dev/CGAL-5.3-win64-auxiliary-libraries-gmp-mpfr/auxiliary/gmp/include" CACHE PATH "")
+
+SET(MPFR_LIBRARIES "C:/dev/CGAL-5.3-win64-auxiliary-libraries-gmp-mpfr/auxiliary/gmp/lib/libmpfr-4.lib" CACHE PATH "")
+
+SET(BOOST_ROOT "C:/dev/boost_1_66_0" CACHE PATH "")
+
+SET(ZLIB_INCLUDE_DIR "C:/dev/zlib-1.2.11;C:/dev/zlib-1.2.11/build" CACHE PATH "")
+
+SET(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+SET(CMAKE_CXX_FLAGS "/W3 /GR /EHsc /fp:strict /fp:except- /wd4503 /bigobj /MD /permissive- -DCGAL_INCLUDE_WINDOWS_DOT_H -D_SCL_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_WARNINGS" CACHE STRING "")
+
+SET(CMAKE_CXX_FLAGS_DEBUG "" CACHE STRING "")
+
+SET(CMAKE_CXX_FLAGS_RELEASE "/O2 /Ob2 /DCGAL_NDEBUG" CACHE STRING "")
+
+SET(CGAL_HEADER_ONLY ON CACHE BOOL "")
+
+SET(WITH_GMP ON CACHE BOOL "")
+
+SET(WITH_GMPXX OFF CACHE BOOL "")
+
+SET(WITH_MPFR ON CACHE BOOL "")
+
+SET(WITH_NTL OFF CACHE BOOL "")
+
+SET(CMAKE_GENERATOR "Visual Studio 17 2022" CACHE STRING "")
+
+SET(CMAKE_GENERATOR_TOOLSET "v143" CACHE STRING "")
