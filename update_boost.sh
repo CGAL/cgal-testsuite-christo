60f1defb5d331f40ceb634986a4473e482ea4a2e
Jane Tournois <jane.tournois@geometryfactory.com>
2022-12-21 14:59:44 +0100

    when version of boost changes, bootstrap must be called again

--- update_boost.sh
+++ update_boost.sh
@@ -3,6 +3,7 @@ pushd /cygdrive/c/dev/boost
 git fetch --recurse-submodules origin -j6
 git reset --hard origin/develop
 git submodule update --recursive --jobs 6
+./bootstrap.bat --toolset=vc14
 ./b2.exe -s NO_BZIP2=0 -s BZIP2_SOURCE="C:/dev/bzip2-1.0.8" -s ZLIB_SOURCE="C:/dev/zlib-1.2.11" -s NO_ZLIB=0 -j8 --toolset=msvc-14.3 --stagedir=stage64 address-model=64 link=shared --prefix="install_dir" --with-thread --with-date_time --with-program_options --with-iostreams --with-filesystem --with-system --with-serialization install
 
 #./b2 -j6 --toolset=msvc address-model=64  architecture=x86 link=shared --prefix="C:/dev/boost/install_dir" install
